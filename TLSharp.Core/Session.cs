552d4d587a78742f4b0c833e2eab4920ec783ad0
tlsh-amamam <53452737+tlsh-amamam@users.noreply.github.com>
2019-07-30 03:24:46 +0430

    Update Session.cs

--- TLSharp.Core/Session.cs
+++ TLSharp.Core/Session.cs
@@ -54,7 +54,7 @@ namespace TLSharp.Core
 
     public class Session
     {
-	    private const string defaultConnectionAddress = "149.154.175.100";//"149.154.167.50";
+	    private const string defaultConnectionAddress = "149.154.167.50";
 
 		private const int defaultConnectionPort = 443;
 
